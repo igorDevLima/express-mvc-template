6055a24107a972febab0ab12de3476b2a1b9d384
IgorLima <igorlimadrum@gmail.com>
2024-05-28 10:10:21 -0300

    refactor: use files array to write files

--- src/scripts/generateFiles.ts
+++ src/scripts/generateFiles.ts
@@ -1,13 +1,39 @@
+import { Answers } from "../config/questions/interface.js";
+import { asyncWriteFile } from "../functions/asyncWriteFile.js";
 
+//contents
 import { packageJson } from "../contents/packageJson.js";
-import { Answers } from "config/questions/interface.js";
-import { asyncWriteFile } from "functions/asyncWriteFile.js";
+import { envExample } from "../contents/envExample.js";
+import { gitIgnore } from "../contents/gitIgnore.js";
+
+type TemplateFile = {
+  directory: string;
+  content: string;
+}[];
 
 export const generateFiles = async (answers: Answers) => {
+  const files: TemplateFile = [
+    {
+      directory: "/package.json",
+      content: packageJson(answers.language),
+    },
+    {
+      directory: "/.env.example",
+      content: envExample(answers.database),
+    },
+    {
+      directory: "/.gitignore",
+      content: gitIgnore(),
+    },
+  ];
+
   try {
-    await asyncWriteFile(
-      `${answers.directory}/package.json`,
-      packageJson(answers.language)
+    files.map(
+      async (file) =>
+        await asyncWriteFile(
+          `${answers.directory}${file.directory}`,
+          file.content
+        )
     );
   } catch (err) {
     console.error(err);
